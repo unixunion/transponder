6e9d0bef371422a756f49c13db650b0095115202
Kegan Holtzhausen <root@e7e51b2f6a7c.(none)>
2013-12-02 13:14:57 +0000

    removed local interface bindings for docker testing

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
-vertx runzip build/libs/transponder-1.0.0-final.zip -cluster  -cluster-port 25501 -cluster-host 127.0.0.1 -Djava.util.logging.config.file="/opt/vert.x-2.0.1-final/conf/logging.properties"
+#!/bin/sh
+#vertx runzip build/libs/transponder-1.0.0-final.zip -cluster  -cluster-port 25501 -cluster-host 127.0.0.1 -Djava.util.logging.config.file="/opt/vert.x-2.0.1-final/conf/logging.properties"
+. /root/.bashrc
 
+cd /opt/transponder
 
-vertx run src/main/java/com/deblox/mods/transponder/Transponder.java -cluster  -cluster-port 25501 -cluster-host 127.0.0.1 -Djava.util.logging.config.file="/opt/vert.x-2.0.1-final/conf/logging.properties"
+vertx run src/main/java/com/deblox/mods/transponder/Transponder.java -cluster -Djava.util.logging.config.file="/opt/vert.x-2.0.2-final/conf/logging.properties"

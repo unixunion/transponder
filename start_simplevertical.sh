28c5950f301f2b2c50a0c0464faf62e06a9083d4
Kegan Holtzhausen <root@3eaf4617945d.(none)>
2013-12-02 13:46:43 +0000

    fixed chdir

--- start_simplevertical.sh
+++ start_simplevertical.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 . /root/.bashrc
+cd /opt/transponder
 
 vertx run src/main/java/com/deblox/mods/transponder/SimpleVertical.java -cluster -Djava.util.logging.config.file="/opt/vert.x-2.0.2-final/conf/logging.properties"
